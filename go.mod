b6b0a3d01ed8658ab9664f1601f79f9209e5d3cd
Riley Rice <riceriley59@gmail.com>
2025-07-23 21:15:38 -0700

    got simple communication between subprocess and engine via gRPC

--- go.mod
+++ go.mod
@@ -2,9 +2,19 @@ module github.com/riceriley59/synkra
 
 go 1.24.5
 
-require github.com/spf13/cobra v1.9.1
+require (
+	github.com/spf13/cobra v1.9.1
+	google.golang.org/grpc v1.74.2
+	google.golang.org/protobuf v1.36.6
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.7 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
 )
+
+replace github.com/riceriley59/synkra/proto => ./github.com/riceriley59/synkra/proto
--- sdk/python/synkra.py
+++ sdk/python/synkra.py
@@ -1,6 +1,11 @@
 import json
 import sys
 from typing import Dict, Any, List
+import os
+import grpc
+import pingpong_pb2
+import pingpong_pb2_grpc
+import importlib.util
 
 
 class Resource:
@@ -38,3 +43,22 @@ class Context:
 
 # Global context — simulates the Pulumi runtime
 global_context = Context()
+
+
+def register_resource(typ, name, props):
+    global_context.register_resource(typ, name, props)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: synkra.py <user_file.py>")
+        sys.exit(1)
+    user_file = sys.argv[1]
+    # Make register_resource available to the user file
+    globals_dict = globals().copy()
+    globals_dict["register_resource"] = register_resource
+    # Import and execute the user file
+    spec = importlib.util.spec_from_file_location("user_module", user_file)
+    user_module = importlib.util.module_from_spec(spec)
+    user_module.register_resource = register_resource
+    sys.modules["user_module"] = user_module
+    spec.loader.exec_module(user_module)
